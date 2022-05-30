8413f16c3b3387d717277c166e055e418d0c3158
Akuseru <Akuseru@DESKTOP-QTLK0QU>
2022-05-30 14:46:00 -0500

    Agregar archivos de proyecto.

---
+++ WindowsFormsAppArduinoGauge/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WindowsFormsAppArduinoGauge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.valueLabel = new System.Windows.Forms.Label();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.valueLabel2 = new System.Windows.Forms.Label();
+            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.userControl11 = new WindowsFormsAppArduinoGauge.UserControl1();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM3";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(231, 265);
+            this.trackBar1.Maximum = 90;
+            this.trackBar1.Minimum = -90;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(354, 45);
+            this.trackBar1.TabIndex = 1;
+            // 
+            // valueLabel
+            // 
+            this.valueLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.valueLabel.Location = new System.Drawing.Point(592, 115);
+            this.valueLabel.Name = "valueLabel";
+            this.valueLabel.Size = new System.Drawing.Size(151, 55);
+            this.valueLabel.TabIndex = 2;
+            this.valueLabel.Text = "Value";
+            this.valueLabel.Click += new System.EventHandler(this.valueLabel_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(374, 300);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 3;
+            this.buttonExit.Text = "Salir";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // valueLabel2
+            // 
+            this.valueLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.valueLabel2.Location = new System.Drawing.Point(592, 199);
+            this.valueLabel2.Name = "valueLabel2";
+            this.valueLabel2.Size = new System.Drawing.Size(151, 55);
+            this.valueLabel2.TabIndex = 2;
+            this.valueLabel2.Text = "Value";
+            this.valueLabel2.Click += new System.EventHandler(this.valueLabel_Click);
+            // 
+            // elementHost1
+            // 
+            this.elementHost1.Location = new System.Drawing.Point(149, 31);
+            this.elementHost1.Name = "elementHost1";
+            this.elementHost1.Size = new System.Drawing.Size(522, 398);
+            this.elementHost1.TabIndex = 0;
+            this.elementHost1.Text = "elementHost1";
+            this.elementHost1.Child = this.userControl11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.valueLabel2);
+            this.Controls.Add(this.valueLabel);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.elementHost1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.SystemColors.GrayText;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private UserControl1 userControl11;
+        private System.Windows.Forms.Timer timer1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label valueLabel;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label valueLabel2;
+    }
+}
+
---
+++ WindowsFormsAppArduinoGauge/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace WindowsFormsAppArduinoGauge
+{
+    public partial class Form1 : Form
+    {
+        int times;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            serialPort1.Open();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            userControl11.rotation();
+            string cadena;
+            int farenheints;
+            if (serialPort1.IsOpen)
+            {
+                cadena = serialPort1.ReadLine();
+                Int32.TryParse(cadena, out times);
+                userControl11.angle = times;
+                trackBar1.Value = times;
+                valueLabel.Text = times.ToString() + (" C");
+                farenheints = (times * 9 / 5) + 32;
+                valueLabel2.Text = farenheints.ToString() + (" F");
+            }
+        }
+
+        private void valueLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
